85652c2317221b80041994824790749249a372be
jwoodard <jonathan@jlwoodard.com>
2014-12-19 12:44:47 -0600

    12-19-14

--- noaa.py
+++ noaa.py
@@ -6,16 +6,27 @@ from lxml import etree
 root = etree.parse("http://forecast.weather.gov/MapClick.php"
                    "?lat=35.8916&lon=-90.65833299999997&unit=0&lg=english&FcstType=dwml")
 
+# root = etree.parse('forecast_sample.xml')
+
 layoutKeys = {}
 
+
 for timeLayouts in root.findall('.//time-layout'):
-    layoutKeys[timeLayouts.find('layout-key').text] = {}
+    layoutKeys[timeLayouts.find('layout-key').text] = []
 
     for StartValidTime in timeLayouts.findall('start-valid-time'):
-        layoutKeys[timeLayouts.find('layout-key').text][StartValidTime.get('period-name')] = []
+        test = [StartValidTime.get('period-name')]
+        layoutKeys[timeLayouts.find('layout-key').text].append(test)
 
-for times in root.findall('.//temperature/value'):
-    print times.text
+temps1 = {}
+for temps in root.findall('.//temperature'):
+    i = 0
+    for test in temps.findall("value"):
+        print layoutKeys[temps.get('time-layout')][i].append(test.text)
+        print str(i) + ' ' + test.text
+        #print test.text
+        i += 1
+    print temps.get('time-layout')
+    # print test
 
-
-print 'this is a change'
+print 'end'
