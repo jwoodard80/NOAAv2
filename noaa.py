cb587017d37a3c6025feb4c339c830c369208fcc
Phyllis <jonathan@jlwoodard.com>
2014-12-25 22:04:24 -0600

    Update

--- noaa.py
+++ noaa.py
@@ -29,4 +29,29 @@ for temps in root.findall('.//temperature'):
     print temps.get('time-layout')
     # print test
 
+# re-format this as a function
+
+for precip in root.findall('.//probability-of-precipitation'):
+    i = 0
+    for rain in precip.findall('value'):
+        if rain.text is not None:
+            layoutKeys[precip.get('time-layout')][i].append(rain.text)
+        else:
+            layoutKeys[precip.get('time-layout')][i].append(str(0))
+        i += 1
+
+for x in root.findall('.//weather'):
+    i = 0
+    for conditions in x.findall('weather-conditions'):
+        if conditions.get('weather-summary'):
+            layoutKeys[x.get('time-layout')][i].append(conditions.get('weather-summary'))
+        i += 1
+
+for x in root.findall('wordedForecast'):
+    i = 0
+    for forecast in x.findall('text'):
+        layoutKeys[x.get('time-layout')][i].append(forecast.text)
+        i += 1
+
+
 print 'end'
